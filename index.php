a85888c2cf83c0f669269c559a51b7c78457361e
Abbysugar <bukksbisuga@gmail.com>
2017-07-13 16:17:25 +0100

    file size and image type

--- index.php
+++ index.php
@@ -1,33 +1,31 @@
 <?php
-	/**
-	 * checks if the selected image is in the array,
-	 * extracts the file path and moves the uploaded 
-	 * image into the filepath.
-	 */
-	if (isset($_FILES['image'])) {
-		
-		$directory = 'uploads/';
-				
-		$filepath = $directory . basename($_FILES["image"]["name"]);
 
-		$imageUpload = move_uploaded_file($_FILES["image"]["tmp_name"], $filepath);
+function imageUpload()
+{
+    $directory = 'uploads/';
+    $filepath  = $directory . basename($_FILES["image"]["name"]);
+    $jpgforce  = pathinfo($filepath, PATHINFO_EXTENSION);
+    if (empty($_FILES['image']['name'])) {
+        $message = "No file uploaded!";
+    } else if ($jpgforce != '.jpg') {
+        $message = "All images MUST be .jpg format";
+    } else if ($_FILES["image"]["size"] > 500000) {
+        echo "Sorry, your image is too large.";
+    } else if (file_exists($filepath)) {
+        $message = "Sorry, image name already exists!";
+    } else {
+        $imageUpload = move_uploaded_file($_FILES["image"]["tmp_name"], $filepath);
+        if ($imageUpload) {
+            $message = "Image uploaded successfully!!!";
+        }
+    }
 
-		if ($imageUpload)
-			echo 'Image uploaded successfully';
-		else
-			echo 'Image not uploaded';
-	}
-?>
+    return $message;
+}
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Image Upload</title>
-</head>
-<body>
-	<form action="" method="POST" enctype="multipart/form-data">
-		<input type="file" name="image">
-		<button type="submit">Submit</button>
-	</form>
-</body>
-</html>
+
+require 'index.html';
+
+if (isset($_POST['submit'])) {
+    echo imageUpload();
+}
